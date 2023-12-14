e04589f34b37f84eedbc67718c689c17d22c8c92
riinavi <riinavi86@gmail.com>
2023-12-14 23:06:03 +0200

    add homework#12

--- hw12/initial.sh
+++ hw12/initial.sh
@@ -1,5 +1,10 @@
+#in Master
 sudo hostnamectl set-hostname "k8smaster"
+
+#in Worker
 #sudo hostnamectl set-hostname "k8sworker"
+
+#in Both
 exec bash
 
 sudo tee --append /etc/hosts << EOF
@@ -54,7 +59,7 @@ sudo apt-mark hold kubelet kubeadm kubectl
 
 #in Master
 sudo kubeadm init
-#sudo kubeadm init --control-plane-endpoint=k8smaster
+
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -65,37 +70,26 @@ kubectl get nodes
 sudo kubeadm join 172.31.88.94:6443 --token f5oy55.zd0b4e7a2ri60w7u \
         --discovery-token-ca-cert-hash sha256:b7d034cd32b8add26a571ce7950d4f0c45bf7eb213ff7127eca046d620d6c1e1
 
-
 # in Master
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
-kubectl create deployment nginx-app --image=nginx --replicas=2
 
+#create nginx deployment
+kubectl create deployment nginx-app --image=nginx --replicas=2
 kubectl expose deployment nginx-app --type=NodePort --port=80
 kubectl get svc nginx-app
 
+#create secret
 kubectl create secret generic mysql-secret --from-literal=mysql-user=admin --from-literal=mysql-password=passpord123
 
-#kubectl run mysql-deployment --image=mysql:latest --env="MYSQL_ROOT_PASSWORD=$(kubect1 get secret mysql-secret -n my-namespace -o=jsonpath='{.data.mysql-password}' | base64 - -decode)" --env="MYSQL_DATABASE=mydb" --env="MYSQL_USER=$(kubectl get secret mysql-secret -n my-namespace -o=jsonpath='{.data.mysql-user}' | base64 --decode)" --env="MYSQL_PASSWORD=$(kubectl get secret mysql-secret -n my-namespace) -o=jsonpath='{•data.mysql-password}' | base64 --decode)"
-
-
-#TODO creaate namespace
+#create namespace
 kubectl create namespace riinavi-namespace
 
-#kubectl run mysql-deployment --image=mysql:latest --env="MYSQL_ROOT_PASSWORD=$(kubectl get secret mysql-secret -n riinavi-namespace -o=jsonpath='{.data.mysql-password}' | base64 --decode)" --env="MYSQL_DATABASE=mydb" --env="MYSQL_USER=$(kubectl get secret mysql-secret -n riinavi-namespace -o=jsonpath='{.data.mysql-user}' | base64 --decode)" --env="MYSQL_PASSWORD=$(kubectl get secret mysql-secret -n riinavi-namespace -o=jsonpath='{data.mysql-password}' | base64 --decode)"
-
-
-
+#create mysql deployment
 kubectl run mysql-deployment --image=mysql:latest --env="MYSQL_ROOT_PASSWORD=$(kubectl get secret mysql-secret -o=jsonpath='{.data.mysql-password}' | base64 --decode)" --env="MYSQL_DATABASE=mydb" --env="MYSQL_USER=$(kubectl get secret mysql-secret -o=jsonpath='{.data.mysql-user}' | base64 --decode)" --env="MYSQL_PASSWORD=$(kubectl get secret mysql-secret -o=jsonpath='{data.mysql-password}' | base64 --decode)"
 
-kubectl get pods
+#connect to pod
+kubectl exec --stdin --tty mysql-deployment -- /bin/bash
+mysql -p
 
-
-#TODO connect to database
-
-kubectl exec --stdin --tty mysql-deployment-1 -- /bin/bash
-kubectl exec -it mysql-deployment-1 -- /bin/bash
-
-#TODO delete everything
+#delete everything
 kubectl delete all --all
-kubectl delete -k ./
-kubectl delete namespaces riinavi-namespace
