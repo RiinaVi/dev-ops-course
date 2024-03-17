4736a0c4ad8a6c08d33e03bab2e3f398e4ec7884
riinavi <riinavi86@gmail.com>
2024-03-17 21:35:54 +0200

    add homework 29

---
+++ hw-29/initial.sh
@@ -0,0 +1,58 @@
+#Install docker
+
+sudo apt install ca-certificates curl gnupg wget apt-transport-https -y
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+newgrp docker
+docker -v
+
+#Install kubectl
+
+curl -LO https://dl.k8s.io/release/`curl -LS https://dl.k8s.io/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+kubectl version --client
+
+
+#Intsall minikube
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
+
+minikube version
+
+
+#Start minikube
+
+minikube start --driver=docker
+minikube status
+
+#Configure ArgoCD
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443
+
+kubectl get secret argocd-initial-admin-secret -n argocd -o yaml
+
+echo {{ encodedPassword }} | base64 --decode
+
+#Log in with admin + decoded password
